1d078bd9fdf9697c2910d6b092c2528e90fa96cf
jomapormentilla <jomapormentilla@gmail.com>
2020-11-29 19:03:10 -0500

    Authenticate User at Login Page and maintain session data. Redirects to proper route when applicable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,44 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-        erb :index
+        if session[:loggedin]
+            redirect '/dashboard'
+        else
+            @error = ""
+            erb :login
+        end
+    end
+
+    post '/login' do
+        if session[:loggedin]
+            redirect '/dashboard'
+        else
+            validate = Wizard.all.find{ |user| user.username == params[:username] && user.password == params[:password] }
+
+            if validate != nil
+                session[:user] = validate
+                session[:loggedin] = true
+                @session = session
+
+                redirect '/dashboard'
+            else
+                @error = "Invalid Username or Password"
+                erb :login
+            end
+        end
+    end
+
+    get '/dashboard' do
+        if session[:loggedin]
+            @session = session
+            erb :index
+        else
+            redirect '/'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 end
